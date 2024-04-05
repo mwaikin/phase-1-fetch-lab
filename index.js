c5ca9a2b5b63a6fc60e131a00c29fc397152ad86
mwaikin <mwaikingori10@gmail.com>
2024-04-06 01:38:24 +0300

    completed fetch lab

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response =>{
+    if(!response.ok) {
+      error('response not ok');
+    }
+    return response.json();
 
+  }) 
+  .then(data => {
+    renderBooks(data);
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:',error)
+  });
+}
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const bookList = document.querySelector('book-list');
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const listItem = document.createElement('li');
+    listItem.innerHTML = book.name;
+    main.appendChild(listItem);
   });
 }
 
